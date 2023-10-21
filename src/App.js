4c7c552c60d8c9a8fe547c10578444fb1a834d59
tatianasnook <tatsnook@gmail.com>
2023-10-21 11:55:53 -0500

    add second image

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import React from 'react';
 import Image from './Image';
+import Image2 from './Image2';
 
 function App() {
   return (
     <div className="App">
       <Image />
+      <Image2 />
     </div>
   );
 }
---
+++ src/Image2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import picture from './forest.png';
+
+function Image2() {
+  return (
+    <div>
+      <img src={picture} alt='forest' width="400px"/>
+    </div>
+  )
+}
+
+export default Image2;
